84ead6f2465547cf1966c01ad7446882be950250
Gyt <igyteng@gmail.com>
2018-10-09 13:44:51 +0800

    handle exception

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const git = require('./git');
 
+process.on('unhandledRejection', (reason, p) => {
+  console.error('Unhandled Rejection at: Promise', p, 'reason:', reason);
+});
+
+process.on('uncaughtException', (err) => {
+  console.error('Caught exception:');
+  console.error(err);
+});
+
 const later = require('later');
 const text = 'every 1 min';
 const schedule = later.parse.text(text);
