7022b5a7fc06d3daf94b86c9d73f7e34a8d89e9c
Gyt <igyteng@gmail.com>
2018-10-10 10:07:49 +0800

    markdown

--- telegram.js
+++ telegram.js
@@ -15,7 +15,7 @@ const sendMessage = async message => {
 
 const sendCommitMessage = async (branch, commit) => {
   let message = '';
-  message += commit.author().name() + ' create a new commit in [ ' + branch + ' ] at ' + new Date(commit.time() * 1000) + '\n';
+  message += '*' + commit.author().name() + '* create a new commit in [ *' + branch + '* ] at ' + new Date(commit.time() * 1000) + '\n';
   message += 'sha: ' + commit.sha() + '\n';
   message += 'msg: ' + commit.message() + '\n';
   return rp({
@@ -23,6 +23,7 @@ const sendCommitMessage = async (branch, commit) => {
     body: {
       chat_id: config.group,
       text: message,
+      parse_mode: 'Markdown',
     },
     json: true,
   });
